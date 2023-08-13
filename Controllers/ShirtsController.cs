094a242a7298d982e80bcdcae3965770b7e96a7e
Max Morgan <maxmmorgan93@gmail.com>
2023-08-12 22:21:01 -0500

    deleted minimal api Map.ETC and added in controllers. Adding routing template and passed args in through HTTP verbs.

---
+++ Controllers/ShirtsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIDemoOne.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ShirtsController : ControllerBase
+    {
+        [HttpGet]
+        
+        public string GetShirts()
+        {
+            return "Reading all shirts";
+        }
+
+        [HttpGet("{id}")]
+        
+        public string GetShirtById(int id)
+        {
+            return $"Reading shirt {id}";
+        }
+
+        [HttpPost]
+        
+        public string CreateShirt()
+        {
+            return "Created shirt";
+        }
+
+        [HttpPut("{id}")]
+        
+        public string UpdateShirt(int id)
+        {
+            return $"Updating shirt: {id}";
+        }
+
+        [HttpDelete("{id}")]
+        
+        public string DeleteShirt(int id)
+        {
+            return $"Deleted shirt: {id}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,40 +2,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddControllers();  
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
 
+app.MapControllers();
 
-//Routing with minimal api's
 
-// "/shirts" is the uri. 
-app.MapGet("/shirts", () =>
-{
-    return "Reading all shirts";
-});
 
-app.MapGet("/shirts/{id}", (int id) =>
-{
-    return $"Reading shirt with ID: {id}";
-});
 
-app.MapPost("/shirts", () =>
-{
-    return "Creating a shirt";
-});
 
-app.MapPut("/shirts/{id}", (int id) =>
-{
-    return $"Updating shirt with ID: {id}";
-});
 
-app.MapDelete("/shirts/{id}", (int id) =>
-{
-    return $"Deleting shirt with ID: {id}";
-});
 
 app.Run();
 
